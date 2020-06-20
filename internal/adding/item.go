6dd0337c24eacd137d570657a8898b444b73448e
xabi <xabier.martinez@arex.io>
2020-06-20 21:57:46 +0200

    Create service to add items to a basket

---
+++ internal/adding/item.go
@@ -0,0 +1,70 @@
+package adding
+
+import (
+	"context"
+
+	checkout "github.com/xabi93/lana-test/internal"
+)
+
+//baskets provides access to baskets repository
+type baskets interface {
+	Get(context.Context, checkout.BasketID) (*checkout.Basket, error)
+	Save(context.Context, checkout.Basket) error
+}
+
+//products provides access to products repository
+type products interface {
+	Get(context.Context, checkout.ProductCode) (*checkout.Product, error)
+}
+
+//NewBasketItemsAdded creates a new BasketItemsAdder instance
+func NewBasketItemsAdder(b baskets, p products) *BasketItemsAdder {
+	return &BasketItemsAdder{b, p}
+}
+
+//BasketItemsAdder defines the service to add products to a basket
+type BasketItemsAdder struct {
+	baskets  baskets
+	products products
+}
+
+//Add adds a new item to the basket if the given basket and product exists in the system
+func (a BasketItemsAdder) Add(ctx context.Context, id checkout.BasketID, c checkout.ProductCode) error {
+	b, err := a.getBasket(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	p, err := a.getProduct(ctx, c)
+	if err != nil {
+		return err
+	}
+
+	b.AddItem(p.Code)
+
+	return a.baskets.Save(ctx, *b)
+}
+
+func (s BasketItemsAdder) getBasket(ctx context.Context, id checkout.BasketID) (*checkout.Basket, error) {
+	p, err := s.baskets.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	if p == nil {
+		return nil, checkout.ErrBasketNotExists
+	}
+
+	return p, nil
+}
+
+func (s BasketItemsAdder) getProduct(ctx context.Context, c checkout.ProductCode) (*checkout.Product, error) {
+	p, err := s.products.Get(ctx, c)
+	if err != nil {
+		return nil, err
+	}
+	if p == nil {
+		return nil, checkout.ErrProductNotExists
+	}
+
+	return p, nil
+}
---
+++ internal/adding/item_test.go
@@ -0,0 +1,179 @@
+package adding_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	checkout "github.com/xabi93/lana-test/internal"
+	"github.com/xabi93/lana-test/internal/adding"
+
+	"github.com/xabi93/lana-test/pkg/money"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type basketsMock struct{ mock.Mock }
+
+func (m *basketsMock) Get(ctx context.Context, id checkout.BasketID) (*checkout.Basket, error) {
+	args := m.Called(ctx, id)
+	var b *checkout.Basket
+
+	if args.Get(0) != nil {
+		b = args.Get(0).(*checkout.Basket)
+	}
+
+	return b, args.Error(1)
+}
+
+func (m *basketsMock) Save(ctx context.Context, b checkout.Basket) error {
+	args := m.Called(ctx, b)
+
+	return args.Error(0)
+}
+
+type productsMock struct{ mock.Mock }
+
+func (m *productsMock) Get(ctx context.Context, c checkout.ProductCode) (*checkout.Product, error) {
+	args := m.Called(ctx, c)
+	var b *checkout.Product
+
+	if args.Get(0) != nil {
+		b = args.Get(0).(*checkout.Product)
+	}
+
+	return b, args.Error(1)
+}
+
+type AddingItemsSuite struct {
+	suite.Suite
+
+	ctx context.Context
+
+	service  *adding.BasketItemsAdder
+	baskets  *basketsMock
+	products *productsMock
+
+	basket  checkout.Basket
+	product checkout.Product
+}
+
+func (s *AddingItemsSuite) SetupSuite() {
+	s.basket = checkout.NewBasket()
+	s.product = checkout.Product{
+		Code:  checkout.ProductCode(2),
+		Name:  checkout.ProductName("Lana Pen"),
+		Price: checkout.ProductPrice(money.Eur(99)),
+	}
+}
+
+func (s *AddingItemsSuite) SetupTest() {
+	s.ctx = context.Background()
+
+	s.baskets = new(basketsMock)
+	s.products = new(productsMock)
+
+	s.service = adding.NewBasketItemsAdder(s.baskets, s.products)
+}
+
+func (s AddingItemsSuite) TestFailsCannotGetBasket() {
+	expectedError := errors.New("unexpected error")
+
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(nil, expectedError)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Equal(expectedError, err)
+}
+
+func (s AddingItemsSuite) TestFailsBasketDoesNotExists() {
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(nil, nil)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Equal(checkout.ErrBasketNotExists, err)
+}
+
+func (s AddingItemsSuite) TestFailsWhenCannotGetProduct() {
+	expectedError := errors.New("unexpected error")
+
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(&s.basket, nil)
+
+	s.products.
+		On("Get", s.ctx, s.product.Code).
+		Return(nil, expectedError)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Equal(expectedError, err)
+}
+
+func (s AddingItemsSuite) TestFailsWhenProductNotExists() {
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(&s.basket, nil)
+
+	s.products.
+		On("Get", s.ctx, s.product.Code).
+		Return(nil, nil)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Equal(checkout.ErrProductNotExists, err)
+}
+
+func (s AddingItemsSuite) TestFailsWhenCannotSaveBasket() {
+	expectedError := errors.New("unexpected error")
+
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(&s.basket, nil).
+		On("Save", s.ctx, mock.AnythingOfType("checkout.Basket")).
+		Return(expectedError)
+
+	s.products.
+		On("Get", s.ctx, s.product.Code).
+		Return(&s.product, nil)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Equal(expectedError, err)
+}
+
+func (s AddingItemsSuite) TestSuccessWhenAddsProductToBasketAndSaves() {
+	s.baskets.
+		On("Get", s.ctx, s.basket.ID).
+		Return(&s.basket, nil).
+		On("Save", s.ctx, mock.AnythingOfType("checkout.Basket")).
+		Return(nil)
+
+	s.products.
+		On("Get", s.ctx, s.product.Code).
+		Return(&s.product, nil)
+
+	err := s.service.Add(s.ctx, s.basket.ID, s.product.Code)
+
+	s.Nil(err)
+
+	s.baskets.AssertCalled(s.T(), "Save", s.ctx, mock.MatchedBy(func(b checkout.Basket) bool {
+		if b.ID != s.basket.ID {
+			return false
+		}
+		if len(s.basket.Items) != 1 {
+			return false
+		}
+
+		return s.basket.Items[s.product.Code].Product == s.product.Code
+	}))
+}
+
+func TestAddingItemsSuite(t *testing.T) {
+	suite.Run(t, new(AddingItemsSuite))
+}
