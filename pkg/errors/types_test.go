e91b541b39fadfe7ab1c72152acd7ee4aaab71ea
xabi <xabier.martinez@arex.io>
2020-06-22 11:23:11 +0200

    Add tests to error types

---
+++ pkg/errors/types_test.go
@@ -0,0 +1,51 @@
+package errors_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xabi93/lana-test/pkg/errors"
+)
+
+func TestNewCreatesCorrectErrorType(t *testing.T) {
+	cases := map[string]struct {
+		newFunc   func(format string, args ...interface{}) error
+		checkFunc func(err error) bool
+	}{
+		"wrongInput": {
+			newFunc:   errors.NewWrongInput,
+			checkFunc: errors.IsWrongInput,
+		},
+		"notFound": {
+			newFunc:   errors.NewNotFound,
+			checkFunc: errors.IsNotFound,
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(fmt.Sprintf("%s", name), func(t *testing.T) {
+			require.True(t, c.checkFunc(c.newFunc("some error")))
+		})
+	}
+}
+
+func TestWrapsWithCorrectErrorType(t *testing.T) {
+	cases := map[string]struct {
+		newFunc   func(err error, format string, args ...interface{}) error
+		checkFunc func(err error) bool
+	}{
+		"notFound": {
+			newFunc:   errors.WrapNotFound,
+			checkFunc: errors.IsNotFound,
+		},
+	}
+
+	someError := fmt.Errorf("Some error")
+
+	for name, c := range cases {
+		t.Run(fmt.Sprintf("%s", name), func(t *testing.T) {
+			require.True(t, c.checkFunc(c.newFunc(someError, "wrap error")))
+		})
+	}
+}
